ab605573fe0ceebdf72bd52954da69c644c64a28
Moisés Franco Gutierrez <you@example.com>
2025-07-03 22:25:55 -0600

    [add]: initial structure of app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'search',
+    loadChildren: () => import('./search/search.module').then(m => m.SearchModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
